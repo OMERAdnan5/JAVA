2c4a469ec2214b3ed9a50f8b0013d415909c2630
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-03-11 00:56:02 +0500

    updates

--- Input.java
+++ Input.java
@@ -1,43 +1,43 @@
 import java.util.Scanner;
 public class Input {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-//        System.out.print("Enter your name:");
-//        String name = input.nextLine();//accepts spaces and beyond that
-//        System.out.println("Your name is "+name);
-//        System.out.print("Enter your age:");
-//        int age = Integer.parseInt(input.next()); //take string input and converts it to int
-//        System.out.println("Your age is: "+age);
-//        System.out.print("Enter your height in ft:");
-//        double height = Double.parseDouble(input.next());//take string input and converts it to double
-//        System.out.println("Your height is "+height+" ft");
-//        System.out.println("Are you a student?(true/false):");
-//        boolean is_student = Boolean.parseBoolean(input.next());//take string input and converts it to boolean
-//        System.out.println(name+" is a student: "+is_student);
-//        //0r use input.nextInt(),.nextDouble(),.nextBoolean() they take respective values
-//        System.out.print("Enter a number:");
-//        int a = input.nextInt();
-//        System.out.print("Enter a number with decimal part:");
-//        double b = input.nextDouble();
-//        double c = a * b;
-//        System.out.println("The Product of "+a+" & "+b+" is "+c);
-          //Check common Error:
-//        System.out.println("Are you a boy?!:");
-//        boolean ans = input.nextBoolean(); //when entering true/false there is a \n created which is not accepted by boolean  but by string input
-//        input.nextLine();
-//        System.out.println("Enter a color:");
-//        String color = input.nextLine();
-//        System.out.println(ans+" Your are a boy "+". You like "+color+" color.");
-//      //Determine Area of Triangle;
-//        double breath;
-//        double height;
-//        double Area;
-//        System.out.print("Enter Height of Triangle in cm:");
-//        height = Double.parseDouble(input.next());
-//        System.out.print("Enter Breath of Triangle in cm:");
-//        breath = Double.parseDouble(input.next());
-//        Area = (1.0/2) * (height * breath);
-//        System.out.println("The Area of Triangle with height "+height+" cm & breath "+breath+" cm is "+Area+" cm².");
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter your name:");
+       String name = input.nextLine();//accepts spaces and beyond that
+       System.out.println("Your name is "+name);
+       System.out.print("Enter your age:");
+       int age = Integer.parseInt(input.next()); //take string input and converts it to int
+       System.out.println("Your age is: "+age);
+       System.out.print("Enter your height in ft:");
+       double height = Double.parseDouble(input.next());//take string input and converts it to double
+       System.out.println("Your height is "+height+" ft");
+       System.out.println("Are you a student?(true/false):");
+       boolean is_student = Boolean.parseBoolean(input.next());//take string input and converts it to boolean
+       System.out.println(name+" is a student: "+is_student);
+       //0r use input.nextInt(),.nextDouble(),.nextBoolean() they take respective values
+       System.out.print("Enter a number:");
+       int a = input.nextInt();
+       System.out.print("Enter a number with decimal part:");
+       double b = input.nextDouble();
+       double c = a * b;
+       System.out.println("The Product of "+a+" & "+b+" is "+c);
+        //   Check common Error:
+       System.out.println("Are you a boy?!:");
+       boolean ans = input.nextBoolean(); //when entering true/false there is a \n created which is not accepted by boolean  but by string input
+       input.nextLine();
+       System.out.println("Enter a color:");
+       String color = input.nextLine();
+       System.out.println(ans+" Your are a boy "+". You like "+color+" color.");
+     //Determine Area of Triangle;
+       double breath;
+       double Height;
+       double Area;
+       System.out.print("Enter Height of Triangle in cm:");
+       Height = Double.parseDouble(input.next());
+       System.out.print("Enter Breath of Triangle in cm:");
+       breath = Double.parseDouble(input.next());
+       Area = (1.0/2) * (Height * breath);
+       System.out.println("The Area of Triangle with height "+Height+" cm & breath "+breath+" cm is "+Area+" cm².");
         String item;
         double price,total;
         int qty;
---
+++ whileloop.java
@@ -0,0 +1,80 @@
+public class whileloop {
+    public static void main(String[] args)
+    {
+        //While loop
+        //printing numbers 1 ~ 10
+        int i = 1;
+        System.out.println("Start");
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("End");
+        System.out.println("");
+        //printing numbers 10 ~ 1
+        System.out.println("Start");
+        int j = 10;
+        while(j>0){
+            System.out.println(j);
+            j--;
+        }
+        System.out.println("End");
+        // pattern
+
+        System.out.println("right angle triangle");
+        int a = 0;
+        while(a<=5){
+            // System.out.print("* ");
+            int b = 0;
+            while(b<=a){
+                System.out.print("* ");
+                b++;
+            }
+            System.out.println("");
+            a++;
+        }
+        System.out.println("square");
+        int c = 0;
+        while(c<=5){
+            // System.out.print("* ");
+            int d = 0;
+            while(d<=5){
+                System.out.print("$ ");
+                d++;
+            }
+            System.out.println("");
+            c++;
+        }
+        System.out.println("right angle triangle");
+        int l = 5;
+        while(l>=0){
+            // System.out.print("* ");
+            int m = 0;
+            while(m<=l){
+                System.out.print("* ");
+                m++;
+            }
+            System.out.println("");
+            l--;
+        }
+        System.out.println("Another 1");
+        int x = 0;
+        while(x<5){
+            int y = 0;
+            while(y<x){
+                System.out.print("  ");
+                y++;
+            }
+            int z = 0;
+            while(z<5-x){
+                System.out.print("@ ");
+                z++;
+            }
+            System.out.println("");
+            x++;
+
+        }
+    }
+
+
+}
