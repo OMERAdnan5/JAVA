a51e50b6ed4a9edd754f8718a3674f31ac90d3ec
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-03-16 03:09:25 +0500

    apply

--- switches.java
+++ switches.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class switches {
     public static void main(String[] args) {
-        //switches are an efficient replacement to else if statement
+        //Enhanced switches are an efficient replacement to else if statement (java 14 feature)
         //Gym timing program
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter A Day: ");
@@ -11,15 +11,31 @@ public class switches {
         day=day.toLowerCase();
         day = Character.toUpperCase(day.charAt(0)) + day.substring(1);
         switch(day) {
-           case "Monday" -> System.out.println(day+": 7:00 Am - 10:00 Pm");
-           case "Tuesday" -> System.out.println(day+": 7:00 Am - 10:00 Pm");
-           case "Wednesday" -> System.out.println(day+": 7:00 Am - 10:00 Pm");
-           case "Thursday" -> System.out.println(day+": 7:00 Am - 10:00 Pm");
+           case "Monday","Tuesday", "Wednesday", "Thursday" -> System.out.println(day+": 7:00 Am - 10:00 Pm");
            case "Friday" -> System.out.println(day+": 2:00 Am - 12:00 Pm");
            case "Saturday" -> System.out.println(day+": 7:00 Am - 12:00 Pm");
            case "Sunday" -> System.out.println(day+": Closed");
            default -> System.out.println(day+": Invalid Input!!!");
         }
-        scanner.close();
+        //Calculator using switches
+        System.out.print("Enter a number: ");
+        double n1 = scanner.nextDouble();
+        System.out.print("Enter another number: ");
+        double n2 = scanner.nextDouble();
+        System.out.print("Enter a operator +,-,*,/,^,%: ");
+        String op = scanner.next();
+        switch(op){
+            case "+" -> System.out.printf("The sum of %.2f and %.2f is %.3f",n1,n2,n1+n2);
+            case "-" -> System.out.printf("The difference of %.2f and %.2f is %.3f",n1,n2,n1-n2);
+            case "*" -> System.out.printf("The product of %.2f and %.2f is %.3f",n1,n2,n1*n2);
+            case "^" -> System.out.printf("The power of %.2f raised to %.2f is %.3f",n1,n2,Math.pow(n1,n2));
+            case "/" ->{
+                if(n2!=0){System.out.printf("The division of %.2f by %.2f is %.3f",n1,n2,n1/n2); }
+                else{System.out.println("Denominator can't be 0");}
+            }
+            default -> System.out.println("Invalid Operator");
+       }
+    scanner.close();
     }
 }
+
