baf8c74775ffb3554a45ccb4631bb3106d9eed27
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-03-21 07:46:00 +0500

    objects

---
+++ OBJ.java
@@ -0,0 +1,5 @@
+public class OBJ {
+    String id = "object_1";
+    String made ="21-march -25";
+    String time = "7:42 AM";
+}
---
+++ OBJuse.java
@@ -0,0 +1,8 @@
+public class OBJuse {
+    public static void main(String[] args) {
+        OBJ object = new OBJ();
+        System.out.println(object.id);
+        System.out.println(object.made);
+        System.out.println(object.time);
+    }
+}
