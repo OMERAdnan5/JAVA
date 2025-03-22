d47e7e2304652a1b19be628b349ee55f3caea4c9
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-03-23 04:01:03 +0500

    Update practice2.java

--- practice2.java
+++ practice2.java
@@ -94,20 +94,58 @@ public class practice2 {
     //             --------
     //             """);
     //Hang man Game!
-    String[] Eng_Q = {"The beautiful flowers are blooming in the garden.(adjective)",
-                      "The book is ___ the table.(preposition)","Yesterday, we ___ to the park.(form of verb)","She ___ her homework before dinner.(form of verb)",
-                      "The cat is sleeping on the couch.(noun)","___ is my best friend.(pronoun)",
-                      "I wanted to go outside, ___ it was raining.(conjuction)"};
-    String [] [] Options ={{"A) flowers","B) are","C) blooming","D) beautiful"},{"A) on","B) in","C) at","D) over"},{"A) go","B) gone","C) went","D) going"},{"A) do","B) did","C) doing","D) does"},{"A) sleeping","B) is","C) cat","D) on"},{"A) He","B) Him", "C) His","D) He's"},{"A) or" ,"B) but","C) and","D) so"}};                 
-    String[] Correct_Ans = {"D", "A", "C", "B", "C", "A", "B"};
+    String[] Eng_Q = {
+        "The beautiful flowers are blooming in the garden.(adjective)",
+        "The book is ___ the table.(preposition)",
+        "Yesterday, we ___ to the park.(form of verb)",
+        "She ___ her homework before dinner.(form of verb)",
+        "The cat is sleeping on the couch.(noun)",
+        "___ is my best friend.(pronoun)",
+        "I wanted to go outside, ___ it was raining.(conjunction)",
+        "The sun ___ in the east.(form of verb)",
+        "The little boy played with his ___ in the park.(noun)",
+        "She sings very ___.(adverb)",
+        "He ___ a new bicycle last week.(form of verb)",
+        "I need to buy ___ eggs from the store.(determiner)",
+        "They will arrive ___ 5 PM.(preposition)",
+        "She ___ a doctor when she was younger.(form of verb)",
+        "The students are writing their exams ___.(adverb)"
+    };
+    
+    String[][] Options = {
+        {"A) flowers", "B) are", "C) blooming", "D) beautiful"},
+        {"A) on", "B) in", "C) at", "D) over"},
+        {"A) go", "B) gone", "C) went", "D) going"},
+        {"A) do", "B) did", "C) doing", "D) does"},
+        {"A) sleeping", "B) is", "C) cat", "D) on"},
+        {"A) He", "B) Him", "C) His", "D) He's"},
+        {"A) or", "B) but", "C) and", "D) so"},
+        {"A) set", "B) rises", "C) rose", "D) raising"},
+        {"A) friends", "B) toys", "C) books", "D) bicycles"},
+        {"A) beautifully", "B) beautiful", "C) beauty", "D) beautifies"},
+        {"A) buy", "B) buys", "C) bought", "D) buying"},
+        {"A) much", "B) some", "C) any", "D) many"},
+        {"A) on", "B) in", "C) at", "D) into"},
+        {"A) was", "B) were", "C) been", "D) is"},
+        {"A) quiet", "B) quick", "C) quietly", "D) quicker"}
+    };
+    
+    String[] Correct_Ans = {"D", "A", "C", "B", "C", "A", "B", "B", "B", "A", "C", "B", "C", "A", "C"};
+    
     int correct = 0;
+    int strike = 0;
     for(int i = 0;i<Eng_Q.length;i++){
-        System.out.println("Q"+(i+1)+". "+Eng_Q[i]);
+        if(strike == 4){
+            System.err.println("\nYou Lost!!!!");
+            System.out.println("Due to "+strike+" strikes.");
+            break;
+        }
+        System.out.println("\nQ"+(i+1)+". "+Eng_Q[i]);
         System.err.println("\nOPTIONS");
         for(int j=0;j<4;j++){ //j<4 for 0 1 2 3 options A-D
            System.out.println(Options[i][j]);
         }
-        System.out.println("\nEnter your answer (A,B,C,D): ");
+        System.out.print("\nEnter your answer (A,B,C,D): ");
         String user_Ans = scanner.next().toUpperCase();
         if(user_Ans.equals(Correct_Ans[i])){
            System.out.println("\"Correct\"");
@@ -115,9 +153,63 @@ public class practice2 {
         }
         else{
             System.out.println("Incorrect");
+            strike++;
+            hang_man(strike);
         }
-
     }
-    System.out.println("Your score is"+correct+"/"+Eng_Q.length);
+    System.out.println("Your score is "+correct+"/"+Eng_Q.length);
+    }
+    static void hang_man(int num){
+       switch(num){
+        case 1 -> System.out.println("""
+
+                 *
+                 * 
+                 * 
+    
+                """);
+       case 2 -> System.out.println("""
+
+                 *
+                 * 
+                 * 
+                * * 
+              *     *   
+              *     * 
+                * *  
+                             """);
+       case 3 -> System.out.println("""
+
+                 *
+                 * 
+                 * 
+                * * 
+              *     *   
+              *     * 
+                * *          
+             *   *   *
+              *  *  *
+               * * *
+                 * 
+                  """);       
+       case 4 -> System.out.println("""
+
+                 *
+                 * 
+                 * 
+                * * 
+              *     *   
+              *     * 
+                * *          
+             *   *   *
+              *  *  *
+               * * *
+                 * 
+                 *    
+                * *
+               *   *
+              *     *
+                      """);      
+       }
     }
 }
