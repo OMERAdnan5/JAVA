d87f736a3082cc574cb29bf6e2fde75712b07080
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-03-23 05:57:56 +0500

    Update practice2.java

--- practice2.java
+++ practice2.java
@@ -4,96 +4,96 @@ public class practice2 {
         //Dice roller
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
-    //     boolean run =true;
-    //     while(run){
-    //        System.out.print("Enter number of dice: ");
-    //        int num = scanner.nextInt();
-    //        int total = 0;
-    //        switch(num){
-    //         case 1 ->System.out.println("You choose 1 dice");
-    //         case 2 ->System.out.println("You choose 2 dice");
-    //         case 3 ->System.out.println("You choose 3 dice");
-    //         case 4 ->System.out.println("You choose 4 dice");
-    //         case 5 ->System.out.println("You choose 5 dice");
-    //         case 6 ->System.out.println("You choose 6 dice");
-    //         default -> System.out.println("Invalid Input");
-    //        }
-    //        for(int i = 1;i<=num;i++){
-    //         int random = rand.nextInt(1,7);
-    //         total+=random;
-    //         System.out.println("You got: "+random);
-    //         switch(random){
-    //             case 1 ->dice1();
-    //             case 2 ->dice2();
-    //             case 3 ->dice3();
-    //             case 4 ->dice4();
-    //             case 5 ->dice5();
-    //             case 6 ->dice6();
-    //            }
+        boolean run =true;
+        while(run){
+           System.out.print("Enter number of dice: ");
+           int num = scanner.nextInt();
+           int total = 0;
+           switch(num){
+            case 1 ->System.out.println("You choose 1 dice");
+            case 2 ->System.out.println("You choose 2 dice");
+            case 3 ->System.out.println("You choose 3 dice");
+            case 4 ->System.out.println("You choose 4 dice");
+            case 5 ->System.out.println("You choose 5 dice");
+            case 6 ->System.out.println("You choose 6 dice");
+            default -> System.out.println("Invalid Input");
+           }
+           for(int i = 1;i<=num;i++){
+            int random = rand.nextInt(1,7);
+            total+=random;
+            System.out.println("You got: "+random);
+            switch(random){
+                case 1 ->dice1();
+                case 2 ->dice2();
+                case 3 ->dice3();
+                case 4 ->dice4();
+                case 5 ->dice5();
+                case 6 ->dice6();
+               }
 
-    //        }
-    //        System.out.println("Total: "+total);
-    //        System.out.print("Do you want to roll again(Y/N):");
-    //        String option = scanner.next().toUpperCase();
-    //        run = option.equals("Y");
+           }
+           System.out.println("Total: "+total);
+           System.out.print("Do you want to roll again(Y/N):");
+           String option = scanner.next().toUpperCase();
+           run = option.equals("Y");
            
-    //     }
-    //     scanner.close();
-    // }
-    // static void dice1(){
-    //     System.out.println( """
-    //             --------
-    //            |        |
-    //                ⨀    
-    //            |        |
-    //             --------
-    //             """);
-    // }
-    // static void dice2(){
-    //     System.out.println("""
-    //             --------
-    //            |        |
-    //              ⨀   ⨀  
-    //            |        |
-    //             --------
-    //             """);
-    // }
-    // static void dice3(){
-    //     System.out.println("""
-    //             --------
-    //            | ⨀     |
-    //            |   ⨀   |
-    //            |     ⨀ |
-    //             --------
-    //             """);
-    // }
-    // static void dice4(){
-    //     System.out.println("""
-    //             --------
-    //            |⨀    ⨀|
+        }
+        scanner.close();
+    }
+    static void dice1(){
+        System.out.println( """
+                --------
+               |        |
+                   ⨀    
+               |        |
+                --------
+                """);
+    }
+    static void dice2(){
+        System.out.println("""
+                --------
+               |        |
+                 ⨀   ⨀  
+               |        |
+                --------
+                """);
+    }
+    static void dice3(){
+        System.out.println("""
+                --------
+               | ⨀     |
+               |   ⨀   |
+               |     ⨀ |
+                --------
+                """);
+    }
+    static void dice4(){
+        System.out.println("""
+                --------
+               |⨀    ⨀|
                         
-    //            |⨀    ⨀|
-    //             --------
-    //             """);
-    // }
-    // static void dice5(){
-    //     System.out.println("""
-    //             --------
-    //            |⨀    ⨀|
-    //                ⨀    
-    //            |⨀    ⨀|
-    //             --------
-    //             """);
-    // }
-    // static void dice6(){
-    //     System.out.println("""
-    //             --------
-    //            |⨀     ⨀|
-    //            |⨀     ⨀|     
-    //            |⨀     ⨀|
-    //             --------
-    //             """);
-    //Hang man Game!
+               |⨀    ⨀|
+                --------
+                """);
+    }
+    static void dice5(){
+        System.out.println("""
+                --------
+               |⨀    ⨀|
+                   ⨀    
+               |⨀    ⨀|
+                --------
+                """);
+    }
+    static void dice6(){
+        System.out.println("""
+                --------
+               |⨀     ⨀|
+               |⨀     ⨀|     
+               |⨀     ⨀|
+                --------
+                """);
+    Hang man Game!
     String[] Eng_Q = {
         "The beautiful flowers are blooming in the garden.(adjective)",
         "The book is ___ the table.(preposition)",
