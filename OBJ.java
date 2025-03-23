2344e8e42de4fe7ddc436e44e26e4ba673e0ddb5
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-03-23 13:28:25 +0500

    objects

--- OBJ.java
+++ OBJ.java
@@ -2,4 +2,9 @@ public class OBJ {
     String id = "object_1";
     String made ="21-march -25";
     String time = "7:42 AM";
+    boolean isname = false;
+    void name(String x){
+       System.out.println("Your name is "+x);
+       isname = true;
+    }
 }
--- OBJuse.java
+++ OBJuse.java
@@ -1,8 +1,21 @@
 public class OBJuse {
     public static void main(String[] args) {
         OBJ object = new OBJ();
+        //object are block of code which can hold data (attributes) & actions (methods)they are a reference data type
+        System.out.println("Object");
         System.out.println(object.id);
         System.out.println(object.made);
         System.out.println(object.time);
+        System.out.println(object.isname);
+        object.name("Umer");
+        System.out.println(object.isname);
+        OBJ example = new OBJ(); // the objects have same properties
+        System.out.println("Example");
+        System.out.println(example.id);
+        System.out.println(object.made);
+        System.out.println(example.time);
+        System.out.println(example.isname);
+        object.name("Adnan");
+        System.out.println(example.isname);
     }
 }
