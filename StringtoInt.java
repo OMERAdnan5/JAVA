69dbdbcff1a33794f5486b99fe332ee284a5a871
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-03-24 10:15:39 +0500

    Create StringtoInt.java

---
+++ StringtoInt.java
@@ -0,0 +1,70 @@
+
+import java.util.Scanner;
+
+public class StringtoInt {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("i.e; Four Hundred Thousand");
+        System.err.print("Enter number:");
+        String str = scn.nextLine();
+        String trimed = str.trim();
+        System.out.println(trimed);
+        System.out.println(trimed.contains(" "));
+        System.out.println(trimed.contains("hundred"));
+        System.err.println(trimed.indexOf(" "));
+        System.err.println(trimed.lastIndexOf(" "));
+        String first = trimed.substring(0,trimed.indexOf(" ")).toLowerCase();
+        String second = trimed.substring(trimed.indexOf(" ")+1).toLowerCase();
+        System.out.println(first);
+        System.out.println(second);
+        int f = 0;
+        int s = 0;
+        switch(first){
+            case "one" -> f = 1;
+            case "two" -> f = 2;
+            case "three" -> f = 3;
+            case "four" -> f = 4;
+            case "five" -> f = 5;
+            case "six" -> f = 6;
+            case "seven" -> f = 7;
+            case "eight" -> f = 8;
+            case "nine" -> f = 9;
+            case "ten" -> f = 10;
+            case "eleven" -> f = 11;
+            case "twelve" -> f = 12;
+            case "thirteen" -> f = 13;
+            case "fourteen" -> f = 14;
+            case "fifteen" -> f = 15;
+            case "sixteen" -> f = 16;
+            case "seventeen" -> f = 17;
+            case "eighteen" -> f = 18;
+            case "nineteen" -> f = 19;
+            case "twenty" -> f = 20;
+            case "thirty" -> f = 30;
+            case "forty" -> f = 40;
+            case "fifty" -> f = 50;
+            case "sixty" -> f = 60;
+            case "seventy" -> f = 70;
+            case "eighty" -> f = 80;
+            case "ninety" -> f = 90;
+            case "zero" -> f = 0;
+            default -> System.out.println("Invalid Input");
+        }
+        switch(second){
+            case "one" -> s = 1;
+            case "two" -> s = 2;
+            case "three" -> s = 3;
+            case "four" -> s = 4;
+            case "five" -> s = 5;
+            case "six" -> s = 6;
+            case "seven" -> s = 7;
+            case "eight" -> s = 8;
+            case "nine" -> s = 9;
+            default -> System.out.println("Invalid Input");
+        }
+        System.out.println(f);
+        System.out.println(s);
+        System.out.println(f+s);
+        
+    }
+}
