a3c48b3b0f5c87d10f6e260750e754b7fd1a89b5
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-03-28 19:37:57 +0500

    Update Static.java

--- Static.java
+++ Static.java
@@ -1,12 +1,15 @@
 public class Static {
-    public static void main(String[] args){
+    public static void main(String[] args){//static helps to create methods and values belonging to the class itself rather then the objects.
         Guest g1 = new Guest("Paul");
         Guest g2 = new Guest("Petter");
         Guest g3 = new Guest("Patric");
         Guest g4 = new Guest("Park");
         Guest g5 = new Guest("Peel");
         g1.arrived();
+        g2.arrived();
+        g3.arrived();
         Guest.totalguests();
+
 
     }
 }
