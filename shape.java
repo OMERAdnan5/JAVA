50440cee00f5dc8d9935cc319adf1c591bc41c93
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-03-29 22:53:56 +0500

    Create shape.java

---
+++ shape.java
@@ -0,0 +1,7 @@
+public abstract  class shape {
+    abstract double area(); //abstract method
+    // abstract method is a must method in all child classes
+    void s(){
+        System.out.println("This is a sub-class of shape. ");
+    }
+}
