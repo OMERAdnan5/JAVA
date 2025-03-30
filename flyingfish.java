ea78aaea28008c9534719f08fd959e1f1642a0ac
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-03-30 15:39:14 +0500

    Create flyingfish.java

---
+++ flyingfish.java
@@ -0,0 +1,11 @@
+public class flyingfish implements fly,swim{
+    public void Fly(){
+       System.out.println("I can fly.");
+    }
+    public void Swim(){
+        System.out.println("I can swim.");
+     }
+     void intro(){
+        System.out.println("I am a Flyingfish.");
+     }  
+}
