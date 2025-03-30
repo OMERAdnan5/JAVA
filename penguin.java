ce7507147835d08a04865cfd5184914e35b363e9
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-03-30 15:39:49 +0500

    Create penguin.java

---
+++ penguin.java
@@ -0,0 +1,11 @@
+public class penguin implements walk , swim {
+    public void Walk(){
+        System.out.println("I can walk.");
+     }
+    public void Swim(){
+         System.out.println("I can swim.");
+      }
+    void intro(){
+        System.out.println("I am a Penguin.");
+     } 
+}
