512210ab4f234751992ff3926aa2dbea76eb6645
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-03-31 22:50:56 +0500

    Create polymorphism.java

---
+++ polymorphism.java
@@ -0,0 +1,12 @@
+public class polymorphism {
+    public static void main(String[] args) {
+        ship ship = new ship();
+        plane plane = new plane();
+        car car = new car();
+        //The data type of it's array will be their parent
+        Vehical[] vehicals = {car,plane,ship};
+        for(Vehical vehical : vehicals){
+            vehical.go();
+        }
+    }
+}
