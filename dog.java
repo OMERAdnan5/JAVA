5c8515080541b88e3129061eb5073047eb307466
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-04-01 18:52:47 +0500

    Create dog.java

---
+++ dog.java
@@ -0,0 +1,10 @@
+public class dog extends Pet{
+    @Override
+    void speak(){
+        System.out.println("The Dog barks.");
+    }
+    @Override
+    void eat(){
+        System.out.println("The Dog eats meat.");
+    }
+}
