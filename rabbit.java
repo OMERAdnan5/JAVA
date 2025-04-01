36f0b9fd79ccc6a96801bc6fc24fd73b82b14026
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-04-01 18:52:27 +0500

    Create rabbit.java

---
+++ rabbit.java
@@ -0,0 +1,10 @@
+public class rabbit extends Pet {
+    @Override
+    void speak(){
+        System.out.println("The Rabbit say bun bun.");
+    }
+    @Override
+    void eat(){
+        System.out.println("The Rabbit eats carrots.");
+    }
+}
