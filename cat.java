f921092339f884f5abf4ced7a3d1323abcae4724
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-04-01 18:52:59 +0500

    Create cat.java

---
+++ cat.java
@@ -0,0 +1,10 @@
+public class cat extends Pet {
+    @Override
+    void speak(){
+        System.out.println("The Cat Meows.");
+    }
+    @Override
+    void eat(){
+        System.out.println("The Cat eats meat.");
+    }
+}
