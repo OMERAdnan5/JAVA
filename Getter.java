1def3f01ccb2aaa3d29a4bd4b16937effc8f26d0
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-04-02 20:07:45 +0500

    Delete Getter.java

--- Getter.java
+++
@@ -1,6 +0,0 @@
-public class Getter {
-    public static void main(String[] args) {
-        car_obj car1 = new car_obj("Lambo","Cyan",245000);
-        System.out.println("The "+car1.getColor()+" "+car1.getModel()+" costs"+car1.getPrice());
-    }
-}
